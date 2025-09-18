1d80e387579f0dadfc547a972fcd3fd914c9b15e
tom anderson <342234136@qq.com>
2025-09-18 18:54:32 +0800

    convert path funciton

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function convertPath(text: string): string {
+	let newPath = text.replace(/\\/g, "/");
+
+	return newPath;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,14 +19,25 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('paste-windows-url.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from paste windows url!');
+	const disposable = vscode.commands.registerCommand('paste-windows-url.paste', async () => {
+
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		const clipboardText = await vscode.env.clipboard.readText();
+
+		const convertedPath = convertPath(clipboardText);
+
+		editor.edit(editBuilder => {
+			const position = editor.selection.active;
+			editBuilder.insert(position, convertedPath);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
